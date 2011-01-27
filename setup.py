83d4928ad622b18a540ec786194694d95123042e
JeanMichel FRANCOIS aka toutpt <toutpt@gmail.com>
2011-01-27 20:41:16 +0100

    add dependencies

--- setup.py
+++ setup.py
@@ -3,6 +3,60 @@ import os
 
 version = '1.0dev'
 
+install_requires=[
+  'setuptools',
+  'gdata',
+  'python-memcached',
+  'suds',
+
+#deps, setup related
+  'collective.autopermission',
+  'collective.googleloader',
+  'collective.monkeypatcher',
+  'plone.app.caching',
+  'plone.app.theming',
+  'five.grok',
+  'Products.CMFPlacefulWorkflow',
+  'Products.contentmigration',
+  'Products.MemcachedManager',
+  'Products.TextIndexNG3',
+  'z3c.unconfigure',
+
+#form
+  'Products.PloneFormGen',
+  'collective.z3cform.norobots',
+  'plone.formwidget.autocomplete',
+  'plone.formwidget.captcha',
+  'archetypes.referencebrowserwidget',
+  'archetypes.uploadreferencewidget',
+  'Products.CompoundField',
+
+#email features
+  'collective.contentrules.mail',
+  'Products.EasyNewsletter',
+  #'collective.dancing', #collective.singing, five.intid, cssutils, BeautifulSoup, zope.sendmail, zope.keyreference>=3.6.2, zope.app.keyreference==3.6.1',zope.app.catalog', zope.app.i18n', zc.queue', zc.lockfile', plone.z3cform>=0.5.1'
+
+#media (photo, audio, video, google maps)
+  'collective.dewplayer',
+  'collective.gallery',
+  'collective.picnik',
+  'p4a.videoembed',
+  'Products.Maps',
+
+
+#menu
+  'collective.portlet.actions',
+  'qi.portlet.TagClouds',
+
+#social network, RSS,...
+  'collective.portlet.feedmixer',
+  'collective.sharerizer',
+
+#others 
+  'collective.portlet.contact',
+  'collective.sugarcrm',
+
+]
 setup(name='toutpt.bundle',
       version=version,
       description="A bundle of add-ons",
@@ -23,10 +77,7 @@ setup(name='toutpt.bundle',
       namespace_packages=['toutpt'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
+      install_requires=install_requires,
       entry_points="""
       # -*- Entry points: -*-
 
